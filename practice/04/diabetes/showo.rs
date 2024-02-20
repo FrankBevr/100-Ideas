b45b29fc8490269e57c179753d2e0d3c7a08d965
FrankBevr <frank_dierolf@web.de>
2024-02-20 13:15:49 +0100

    :zap: (practice) add 04 diabetes paint

---
+++ practice/04/diabetes/showo.rs
@@ -0,0 +1,53 @@
+#![cfg_attr(not(feature = "std"), no_std, no_main)]
+
+#[ink::contract]
+mod diabetes {
+    use ink::prelude:string::String;
+
+    #[ink(storage)]
+    pub struct Diabetes {
+        value: bool,
+        member: AccountId,
+        leader: AccountId,
+        achievments: String,
+    }
+
+    impl Diabetes {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self { 
+                value: true,
+                member: AccountId::from([0xff,32]),
+                leader: AccountId::from([0xff,32]),
+                achievments: String::from("")
+            }
+        }
+
+        #[ink(message)]
+        pub fn flip(&mut self) {
+            self.value = !self.value;
+        }
+
+        #[ink(message)]
+        pub fn get(&self) -> bool {
+            self.value
+        }
+
+        #[ink(message)]
+        pub fn rewarded(&self)  {}
+
+        #[ink(message)]
+        pub fn post(&self)  {
+            self.achievments = String::from("Great Certifacte from the Fitness Studio");
+        }
+
+        #[ink(message)]
+        pub fn get_leader(&self) -> AccountId  {
+            self.leader
+        }
+
+        #[ink(message)]
+        pub fn fund(&self)  {}
+
+    }
+}
